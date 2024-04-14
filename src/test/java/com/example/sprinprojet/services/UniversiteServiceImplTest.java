4cc8624a12f9b510d3aac17a2bf0a75dc86a368b
Meddebsofien <sofien.meddeb@esprit.tn>
2024-04-14 18:13:47 +0200

    test

---
+++ src/test/java/com/example/sprinprojet/services/UniversiteServiceImplTest.java
@@ -0,0 +1,36 @@
+package com.example.sprinprojet.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UniversiteServiceImplTest {
+
+    @Test
+    void retrieveAllUniversites() {
+    }
+
+    @Test
+    void getbyidUni() {
+    }
+
+    @Test
+    void addUniversites() {
+    }
+
+    @Test
+    void updateUniversites() {
+    }
+
+    @Test
+    void retrieveUniversites() {
+    }
+
+    @Test
+    void removeUniversites() {
+    }
+
+    @Test
+    void affecterFoyerAUniversite() {
+    }
+}
